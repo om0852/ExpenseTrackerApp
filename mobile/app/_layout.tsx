77adcfee12ba17d83d1fd111767581436de658de
om salunke <omsaunke343@gmail.com>
2025-07-23 23:22:50 +0530

    app stepup done

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
-
+import SafeScreen from "@/components/SafeScreen"
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeScreen>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+      ;
+    </SafeScreen>
+  );
 }
---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { COLORS } from "@/constants/Colors";
+
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+        backgroundColor: COLORS.background,
+        paddingBottom:insets.bottom
+      }}
+    >
+      {{ children }}
+    </View>
+  );
+};
+
+export default SafeScreen;
+
+const styles = StyleSheet.create({});
