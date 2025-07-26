6133cd6d8ce4dcbf4482e5cd0573f0dc4587cf6b
om salunke <omsaunke343@gmail.com>
2025-07-26 20:28:02 +0530

    backend updated

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -1,7 +1,16 @@
-import { Redis } from "@upstash/redis";
-import {  Ratelimit } from "@upstash/ratelimit";
-import "dotenv/config";
-const ratelimit = new Redis({ redis: Redis.fromEnv(), limiter: Ratelimit.slidingWindow(100,"60 s")});
+import { Redis } from '@upstash/redis';
+import { Ratelimit } from '@upstash/ratelimit';
+import 'dotenv/config';
 
+const redis = Redis.fromEnv();
 
-export default ratelimit
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(100, '60 s'),
+  // optionally:
+  // analytics: true,
+  // prefix: '@upstash/ratelimit',
+  // timeout: 5000
+});
+
+export default ratelimit;
--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
   try {
-    const { success } = await rateLimiter.limit("my-rate-limit");
+    const { success } = await ratelimit.limit("my-rate-limit");
     if (!success) {
       return res.status(429).json({
         message: "Too many requests,please try again later.",
